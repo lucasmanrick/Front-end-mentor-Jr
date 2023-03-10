47032f1ccc56cc44fa521a1416f2339d9887147f
lucasmanrick <112766634+lucasmanrick@users.noreply.github.com>
2023-03-10 00:50:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const childSelector = document.getElementById (`nav-show`)
-  childSelector.addEventListener (`click`, openNavBar)
-
-
-const closeMenuLogo = document.getElementById (`close-png`) 
-  closeMenuLogo.addEventListener (`click`, closeNavBar)
-
-
 const showMenu = document.querySelector (`.menu-side`)
-const subContainer = document.getElementById (`container`)
 
+const openMenuSelector = document.getElementById (`nav-show`)
+openMenuSelector.addEventListener (`click`, openNavBar)
 
 function openNavBar () {
-    showMenu.classList.add (`show`)
-    childSelector.style = "display:none;"
-    subContainer.style = "opacity:0.3;"
+  showMenu.classList.add (`show`)
+  openMenuSelector.parentElement.classList.add (`block`)
+
+  
 }
 
-
+const closeMenuLogo = document.getElementById (`close-png`) 
+closeMenuLogo.addEventListener (`click`, closeNavBar)
 
 function closeNavBar () {
   showMenu.classList.remove (`show`)
-  childSelector.style = "@media (max-width: 960px) {display:block;}"
-  
-  subContainer.style = "opacity:1;"
+  openMenuSelector.parentElement.classList.remove (`block`)
 }
 
 
-subContainer.style = "@media (min-width:660px) opacity:1;"
+
+
+
+
+
+
+
